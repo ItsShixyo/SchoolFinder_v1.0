5637d1d1304bd93e39da9be1d0e6e6c37b58b4a4
tdlma <tdlmabc@gmail.com>
2024-03-19 19:37:51 +0100

    commit

---
+++ M4_Nevada/ECE/main.js
@@ -0,0 +1,48 @@
+let siteLocation = 0;
+let pageIsLoaded = false;
+
+window.addEventListener("wheel", (event) => {
+
+    const container = document.getElementById("container");
+
+    const marginLeft = container.getBoundingClientRect().left;
+
+    if (marginLeft < 0) {
+        document.querySelector("body").style.overflowY = 'unset';
+        if (!pageIsLoaded) {
+            const iframe = document.createElement("iframe");
+            iframe.src = "page1.html";
+            document.querySelector("body").appendChild(iframe);
+            pageIsLoaded = true;
+        }
+    }
+    else {
+        document.querySelector("body").style.overflowY = 'hidden';
+
+        if (pageIsLoaded) {
+            document.querySelector("body").removeChild(document.querySelector("iframe"));
+            pageIsLoaded = false;
+        }
+
+    }
+
+    const fullSite = document.getElementById("full-site");
+
+    let scrollLocation = event.deltaY;
+    scrollLocation > 0 ? (marginLeft > 0 ? siteLocation += 50 : null) : siteLocation -= 50;
+
+    if (siteLocation > 0) {
+        fullSite.style.transform = `translateX(-${siteLocation}px)`;
+    }
+    else {
+        siteLocation = 0;
+    }
+
+    /*
+
+    if (marginLeft < 0) {
+        itemSize += 1;
+    }
+    container.style.transform = `scale(${itemSize})`;
+     */
+});
